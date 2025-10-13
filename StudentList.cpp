fb6d3460ff6662a4a74e38e4be6c1ac23526557b
U-BSD\455138 <455138@SHS-BYBF6X3.beaverton.k12.or.us>
2025-10-13 09:58:14 -0700

    Read in user input for commands

--- StudentList.cpp
+++ StudentList.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <cstring>
+#include <cctype>
 #include <vector>
 
 using namespace std;
@@ -17,6 +18,33 @@ struct Student {
 };
 
 int main() {
+  // create vector of struct pointers
+  vector<Student*> studentList;
 
+  // define const var for commands
+  const char ADD[] = "ADD";
+  const char PRINT[] = "PRINT";
+  const char DELETE[] = "DELETE";
+  const char QUIT[] = "QUIT";
+
+  // declare char arr for user input
+  const int INPUT_LENGTH = 81;
+  char userInput[INPUT_LENGTH];
+
+  // continue prompting user for input until QUIT command
+  bool keepModifying = true;
+  while (keepModifying) {
+    // read in user input and validate
+    cout << "Enter a command (ADD, PRINT, DELETE, QUIT): ";
+    cin.getline(userInput, INPUT_LENGTH);
+
+    if ((strcmp(userInput, ADD) != 0) && (strcmp(userInput, PRINT) != 0) &&
+	(strcmp(userInput, DELETE) != 0) && (strcmp(userInput, QUIT) != 0)) {
+      cout << "Please input ADD, PRINT, DELETE, or QUIT." << endl;
+    } else {
+      cout << "Thank you for reading instructions :)" << endl;
+    }
+  }
+  
   return 0;
 }
