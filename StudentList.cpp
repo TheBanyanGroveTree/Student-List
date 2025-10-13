9d5950dbae539088159b16005fbaa51d83d3cdeb
U-BSD\455138 <455138@SHS-BYBF6X3.beaverton.k12.or.us>
2025-10-13 09:25:22 -0700

    Initial commit

---
+++ StudentList.cpp
@@ -0,0 +1,22 @@
+/* Description:
+   Author: Aahana Sapra
+   Date: 10/13/25
+ */
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+struct Student {
+  char firstName[81];
+  char lastName[81];
+  int id;
+  float gpa;
+};
+
+int main() {
+
+  return 0;
+}
