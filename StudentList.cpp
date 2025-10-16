9f1522fe72f07d7b89d792d3faf22ef11d38d6e1
Aahana Sapra <sapraaahana@gmail.com>
2025-10-15 18:45:25 -0700

    Update addStudent() function by fixing input buffer issue and define printStudentInfo() function

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,12 +1,14 @@
 /* Description:
    Author: Aahana Sapra
-   Date: 10/13/25
+   Date: 10/15/25
  */
 
+// import relevant libraries
 #include <iostream>
-#include <cstring>
-#include <cctype>
 #include <vector>
+#include <cstring>
+#include <limits>
+#include <ios>
 
 using namespace std;
 
@@ -54,9 +56,8 @@ int main() {
       if (strcmp(userInput, ADD) == 0) {
 	addStudent(studentList, INPUT_LENGTH);
       } else if (strcmp(userInput, PRINT) == 0) {
-
+	printStudentInfo(studentList);
       } else if (strcmp(userInput, DELETE) == 0) {
-
       } else {
 	keepModifying = false;
       }
@@ -69,18 +70,37 @@ int main() {
 // define add function to create a new student entry
 void addStudent(vector<Student*> &studentList, const int &INPUT_LENGTH) {
   // read in user input and update student information
-  Student newStudent;
+  // dynamically allocate new struct instance (add to heap)
+  Student* newStudent = new Student();
   cout << "Enter the student's first name: ";
-  cin.getline(newStudent.firstName, INPUT_LENGTH);
+  // -> operator dereferences and accesses obj mbr in one step
+  cin.getline(newStudent->firstName, INPUT_LENGTH);
   
   cout << "Enter the student's last name: ";
-  cin.getline(newStudent.lastName, INPUT_LENGTH);
+  cin.getline(newStudent->lastName, INPUT_LENGTH);
   
   cout << "Enter the student's ID: ";
-  cin >> newStudent.id;
-  cout << endl;
+  cin >> newStudent->id;
+  // QUESTION: how does this work...?
+  cin.ignore(numeric_limits<streamsize>::max(), '\n'); // clear input buffer
 
   cout << "Enter the student's GPA: ";
-  cin >> newStudent.gpa;
-  cout << endl;
+  cin >> newStudent->gpa;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  // add student to studentList
+  studentList.push_back(newStudent);
+}
+
+// define print function that outputs information for all students
+void printStudentInfo(vector<Student*> &studentList) {
+  // QUESTION: why ++it instead of it++?
+  // ALTERNATIVE: range-based for loop
+  for (auto it = studentList.begin(); it != studentList.end(); ++it) {
+    cout.precision(2);
+    // dereference iterator to get to Student* ptr
+    // QUESTION: is an iterator a ptr.. why "dereference it"
+    cout << (*it)->firstName << ' ' << (*it)->lastName << ", ";
+    cout << (*it)->id << ", " << (*it)->gpa << endl; 
+  }
 }
